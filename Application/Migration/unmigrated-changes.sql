05de207ca442a28ed23305ed2c806ec077d5faa4
Hannes Steffenhagen <leexarmatio@gmail.com>
2021-11-19 14:34:23 +0100

    Change some more stuff

---
+++ Application/Migration/unmigrated-changes.sql
@@ -0,0 +1,8 @@
+-- This file is created by the IHP Schema Designer.
+-- When you generate a new migration, all changes in this file will be copied into your migration.
+--
+-- Use http://localhost:8001/NewMigration or `new-migration` to generate a new migration.
+--
+-- Learn more about migrations: https://ihp.digitallyinduced.com/Guide/database-migrations.html
+ALTER TABLE posts DROP COLUMN created_at;
+ALTER TABLE posts ADD COLUMN created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL;
--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,2 +1,7 @@
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
-CREATE TABLE posts(id UUID DEFAULT gen_random_uuid() PRIMARY KEY, title TEXT NOT NULL, body TEXT NOT NULL);
+CREATE TABLE posts (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+);
