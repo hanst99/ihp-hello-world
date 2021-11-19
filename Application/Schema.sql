38887485401d403d286f8d0d11dbd78b6403d649
Hannes Steffenhagen <leexarmatio@gmail.com>
2021-11-19 11:35:19 +0100

    Initial project setup + posts table

---
+++ Application/Schema.sql
@@ -0,0 +1,2 @@
+-- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE posts(id UUID DEFAULT gen_random_uuid() PRIMARY KEY, title TEXT NOT NULL, body TEXT NOT NULL);
